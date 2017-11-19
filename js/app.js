507776404627577796134b0a6e5bead4454a358d
gabrielamf <glennygabb@gmail.com>
2017-11-18 20:10:54 -0500

    añadiendo funcion crecer textarea en app.js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,33 @@
 window.addEventListener('load',function() {
 
-  var area = document.getElementById('texto');
-  var boton = document.getElementById('boton');
-  var lista = document.getElementById('nuevoEspacio')
+  //version 0.0.1
+  var area = document.getElementById('texto'),
+      boton = document.getElementById('boton'),
+      lista = document.getElementById('nuevoEspacio'),
+      restante = document.getElementById('restante');
+
+
   boton.addEventListener('click',showText);
-  area.addEventListener('blur',caracteres);
-  area.addEventListener('keypress',caracteres);
+
+  //version 0.0.2
+  area.addEventListener('keyup',caracteres);
+  area.addEventListener('keyup',darEnter);
+
+
 
   function caracteres() {
-    //var max=new Number();
-    var max = 139;
-    var primer = 119;
-    var segundo = 129;
-    var actual = document.getElementById('texto').value.length;
-    //var valor = new Number();
-    valor = max-actual;c
 
-    if (valor > 0) {
-      //if(valor == 1) {
-        //valor = 0;
-      //}
-      document.getElementById('restante').innerHTML = valor;
-      boton.disabled=''; //habilitar
+    var max = 140;
+    contador = area.value.length;
+    restante.textContent = max - contador;
+
+
+    if (contador >= 120 && contador <= 130){
+      restante.style.color='#ff0000';
+    } else if (contador >= 130 && contador <=140) {
+      restante.style.color= 'blue';
     } else {
-      boton.disabled='disabled'; //desahabillitar
-    }
-
-
-    if (actual >= primer && actual<= segundo){
-      document.getElementById('restante').style.color='#ff0000';
-    } else if (actual >= segundo) {
-      document.getElementById('restante').style.color= 'blue';
+      restante.style.color = 'black';
     }
   }
 
@@ -50,4 +47,22 @@ window.addEventListener('load',function() {
     }
     document.getElementById('restante').textContent = '140';
   }
+
+  function darEnter(){
+  var area = this;
+  //setTimeout(function(){
+    //area.style.cssText = 'height:130px';
+    area.style.cssText = 'height:' + area.scrollHeight + 'px';
+  }
 })
+
+
+/*if (valor > 0) {
+  //if(valor == 1) {
+    //valor = 0;
+  //}
+  document.getElementById('restante').innerHTML = valor;
+  boton.disabled=''; //habilitar
+} else {
+  boton.disabled='disabled'; //desahabillitar
+}*/
